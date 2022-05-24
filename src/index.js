77671ee4b57bf157668590fb87188b0ebe164373
Zoey <nanye@NANYEdeMacBook-Pro.local>
2022-05-24 17:15:53 +0930

    updating Route and navigation component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+const SignIn = () => {
+  return (
+    <Fragment>
+      <div>Sign in page</div>
+    </Fragment>
+  );
+};
+
+export default SignIn;
