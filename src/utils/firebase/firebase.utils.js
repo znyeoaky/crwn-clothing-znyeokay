5c8d435a687e14e1ece4370270199c0fa78bc493
znyeokay <nanye.zou@gmail.com>
2022-05-26 22:19:16 +0930

    finishing authentication page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,7 +6,8 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from 'firebase/auth';
 import {
   getFirestore,
@@ -75,4 +76,11 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if(!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  
+  if(!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
 };
