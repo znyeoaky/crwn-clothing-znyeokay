f8daa178d072db2b86df215b4829c0934a417b37
znyeokay <nanye.zou@gmail.com>
2022-06-05 23:58:49 +0930

    updating thunk and saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,12 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
 
 import { rootReducer } from './root-reducer';
+import { rootSaga } from './root-saga';
 
 
 const persistConfig = {
@@ -18,20 +22,33 @@ const persistConfig = {
   blacklist: an array of strings(root-reducer.js), which reducer you don't want to persist.
 */
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // const middleWares = [logger];
-// production
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+// production // development
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger, 
+  sagaMiddleware].filter(
   Boolean
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancer = (
+  process.env.NODE_ENV !== 'production' && 
+  window && 
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
+compose;
+
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer, 
   undefined, 
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
